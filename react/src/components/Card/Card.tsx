16681421cafba60f4168476416185171bebea7f1
iozefavichus <iozefavichus@gmail.com>
2023-10-29 19:05:26 +0200

    feat: add Error Boundary

---
+++ react/src/components/Card/Card.tsx
@@ -0,0 +1,25 @@
+import { Component } from 'react';
+import styles from './card.module.css';
+import { CardProps } from './CardPropsType';
+
+class Card extends Component<CardProps> {
+  render() {
+    const { id, name, image, status, species, gender } = this.props;
+    return (
+      <div className={styles.card}>
+        <div className={styles.image}>
+          <img src={image} alt={id.toString()} />
+        </div>
+        <div className={styles.info}>
+          <div className={styles.wrapper}>
+            <div className={styles.name}>Name:{name}</div>
+            <div className={styles.status}>Status: {status}</div>
+            <div className={styles.species}>Species: {species}</div>
+            <div className={styles.gender}>Gender: {gender}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export { Card };
